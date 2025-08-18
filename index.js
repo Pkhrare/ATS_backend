a8f28ea4a560613d269b0078cab8cd899e4287cf
prakhar <prakhardeep6@gmail.com>
2025-08-18 08:40:59 -0500

    fix upload 3

--- index.js
+++ index.js
@@ -389,7 +389,7 @@ async function initializeApp() {
         // ----- END API ROUTES -----
 
         // Start the server only after everything is initialized
-        const port = process.env.PORT || 3000;
+        const port = process.env.PORT || 8080;
         server.listen(port, () => {
             console.log(`Server is running on port: ${port}`);
         });
